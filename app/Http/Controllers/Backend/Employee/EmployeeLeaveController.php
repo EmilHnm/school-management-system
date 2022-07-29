1082292ea450feb5caf116d8aa57f6a9787cfefd
EmilRailgun <hoangominh01@gmail.com>
2022-07-30 00:51:19 +0700

    modified:   app/Http/Controllers/Backend/Employee/EmployeeLeaveController.php 	new file:   app/Models/EmployeeLeave.php 	new file:   app/Models/LeavePurpose.php 	new file:   database/migrations/2022_07_29_172144_create_leave_purposes_table.php 	new file:   database/migrations/2022_07_29_173311_create_employee_leaves_table.php 	new file:   resources/views/backend/employee/employee_leave/view_employee_leave.blade.php

--- app/Http/Controllers/Backend/Employee/EmployeeLeaveController.php
+++ app/Http/Controllers/Backend/Employee/EmployeeLeaveController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Backend\Employee;
 
 use App\Http\Controllers\Controller;
+use App\Models\EmployeeLeave;
 use Illuminate\Http\Request;
 
 class EmployeeLeaveController extends Controller
 {
     //
+
+    public function LeaveView()
+    {
+        $data['allData'] = EmployeeLeave::orderBy('id', 'desc')->get();
+
+        return view('backend.employee.employee_leave.view_employee_leave', $data);
+    }
 }
